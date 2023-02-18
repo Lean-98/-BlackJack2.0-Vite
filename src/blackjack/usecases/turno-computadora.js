171d1f753908d10ff53cd59f92aaca84a4a51890
Leandro <leanfrag23@gmail.com>
2023-02-18 16:24:05 -0300

    Deployment

---
+++ src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,78 @@
+import { pedirCarta, valorCarta, crearCartaHTML } from './'; 
+
+/**
+ *  Turno computadora
+ * @param {Number} puntos minimos que la computadora necesita para ganar
+ * @param {HTMLElement} puntosHTML elemento HTML para mostrar los puntos
+ * @param {HTMLElement} divCartasComputadora elemento HTML para mostrar las cartas
+ * @param {Array<String>} deck 
+ */
+
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+    if( !puntosMinimos ) throw new Error('Puntos mínimos son necesarios');
+    if( !puntosHTML ) throw new Error(' Argumento puntosHTML es necesario');
+
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta( deck );
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        const imgCarta = crearCartaHTML( carta );
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora <= puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if ( puntosComputadora === puntosMinimos) {
+            Swal.fire({
+                title: 'TIE!',
+                width: 600,
+                imageWidth: 400,
+                imageHeight: 200,
+                padding: '6rem',
+                color: '#716add',
+                background: '#F0FFF0',
+                imageUrl: 'https://media.giphy.com/media/xT3i0P4CYQcdmFZQkM/giphy.gif',
+                position: 'center',
+                timer: 7000
+              })
+        } else if ( puntosMinimos > puntosComputadora && puntosMinimos <= 21 || puntosComputadora > 21 ) {
+            Swal.fire({
+                title: 'YOU WON!',
+                width: 600,
+                imageWidth: 400,
+                imageHeight: 200,
+                padding: '7rem',
+                color: '#716add',
+                background: '#F0FFF0',
+                imageUrl: 'https://media.giphy.com/media/3ov9jNw01KHVtg4Sd2/giphy.gif',
+                position: 'center',
+                timer: 7000
+              })
+
+        } else if ( puntosComputadora > puntosMinimos && puntosComputadora <= 21 || puntosMinimos > 21 ) {
+            
+            Swal.fire({
+                title: 'YOU LOST!',
+                width: 600,
+                imageWidth: 400,
+                imageHeight: 200,
+                padding: '6rem',
+                color: '#716add',
+                background: '#F0FFF0',
+                imageUrl: 'https://media.giphy.com/media/iG7Kwfxga9r20/giphy.gif',
+                position: 'center',
+                timer: 7000
+              })
+        }
+    }, 1000);
+}
+
